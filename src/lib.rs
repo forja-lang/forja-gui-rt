4656165da0e873d9576c4a82210f500d4f174831
lococoi <lococoi@proton.me>
2026-07-06 04:00:48 -0300

    syntax highliter vsix extension

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,15 @@
 // Este crate se compila UNA SOLA VEZ; las apps generadas solo enlazan contra él.
 
 // Re-exportar todo lo que necesita el código generado por Forja
-pub use xilem::view::{self, Axis, flex, label, text_button, text_input, progress_bar, sized_box};
-pub use xilem::{WidgetView, Xilem, WindowOptions, EventLoop, AnyWidgetView};
+pub use xilem::view::{
+    self, button, button_any_pointer, checkbox, flex, grid, image, indexed_stack, label,
+    portal, progress_bar, prose, sized_box, slider, spinner, split, text_button, text_input,
+    variable_label, virtual_scroll, zstack, Axis, GridParams,
+};
+pub use xilem::{
+    AnyWidgetView, EventLoop, FontWeight, TextAlign, WidgetView, WindowOptions, Xilem,
+};
 pub use xilem::winit::error::EventLoopError;
+pub use xilem::{Affine, Blob, Color, ImageBrush, ImageFormat, palette};
+pub use xilem::core::{lens, memoize};
+pub use xilem::masonry::properties::types::Length;
